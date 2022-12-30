90bcfecb84307ab032ceb6d222f3c5ce27657034
Philipp Hohmann <philipp@hohmann-it.com>
2022-12-30 13:07:47 +0100

    Test2

--- dungeonmaster/dungeonmaster.server/sql/setup.sql
+++ dungeonmaster/dungeonmaster.server/sql/setup.sql
@@ -40,7 +40,6 @@ IF EXISTS dungeonmaster;
         -- Create the new database
         \echo '=========Create dungeonmaster user=========';
         \echo '...';
-        
         CREATE DATABASE dungeonmaster WITH TEMPLATE = template0 ENCODING = 'UTF8';
         \echo '=========Change owner of dungeonmaster database to dungeonmaster user=========';
         \echo '...';
